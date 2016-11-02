31b7095c9910b77d57c74aba815e81820206e8b7
Thomas Foster <thomasfoster90>
2016-11-02 08:47:23 -0500

    deposits

--- src/Account.js
+++ src/Account.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 
-export default class Account extends Component {
+class Account extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      balance:0
+    }
+  }
+  
+  _depositClick(){
+    let newBalance=this.refs.newamount.value;
+    console.log(newBalance)
+    this.setState({
+      balance: this.state.balance + +newBalance
+    })
+  }
+  _withdrawlClick(){
+    let newBalance=this.refs.newamount.value;
+    let currentBalance = this.state.balance;
+    if (newBalance>currentBalance) {
+        alert('You may not overdraw');
+    } else {
+        this.setState({
+          balance:currentBalance - newBalance
+        })
+    }
+  }
+
   render() {
     return (
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <h2>{this.props.name}</h2>
+        <div className="balance">${this.state.balance}</div>
+        <input ref="newamount" type="text" placeholder="enter an amount" />
+        <input onClick={()=> this._depositClick()} type="button" value="Deposit" />
+        <input onClick={(event)=>this._withdrawlClick(event)} type="button" value="Withdrawl" />
       </div>
     )
   }
+  _addBalance(balance){
+    let currentBalance= this.state.balance;
+    let newBalance ={}
+    newBalance.balance=balance;
+    currentBalance.push(newBalance)
+    this.setState({
+      balance:currentBalance
+    })
+  }
 }
+export default Account;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import logo from './ga.png';
-
+import logo from './ga.png'
 import Account from './Account';
 
-class App extends Component {
+class Atm extends Component {
   render() {
     return (
       <div id="content">
@@ -11,12 +10,12 @@ class App extends Component {
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account />
-        <Account />
+        <Account name="Checking"/>
+        <Account name="Savings"/>
         <div className="clear"></div>
       </div>
     );
   }
 }
 
-export default App;
+export default Atm;
